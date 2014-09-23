bd5b66afcda5782fd88ff5d413ddeacf403d3a02
Dara Bun <dara209@gmail.com>
2014-09-22 20:47:27 -0700

    (feat) Style Dashboard

--- client/states/account/services/account.service.spec.js
+++ client/states/account/services/account.service.spec.js
@@ -1,21 +1,21 @@
 describe("Unit: Account", function() {
-	var Account;
+  var Account;
 
-	beforeEach(function () {
-		module('clickaroos.account', function($provide) {
-			$provide.constant('appServerUrl', 'http://APP-SERVER-URL-HERE');
-			});
+  beforeEach(function () {
+    module('clickaroos.account', function($provide) {
+      $provide.constant('appServerUrl', 'http://APP-SERVER-URL-HERE');
+      });
 
-		inject(function ($injector) {
-			Account = $injector.get('Account');
-		})
-	});
+    inject(function ($injector) {
+      Account = $injector.get('Account');
+    })
+  });
 
   it("should have a login function", function() { 
-  	expect(Account.submitLogin).to.be.a('function');
+    expect(Account.submitLogin).to.be.a('function');
   });
 
   it("should have a signup function", function() { 
-  	expect(Account.submitSignup).to.be.a('function');
+    expect(Account.submitSignup).to.be.a('function');
   });
 });
