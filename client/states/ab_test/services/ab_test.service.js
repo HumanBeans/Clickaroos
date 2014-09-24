cb381188743b29afbf1d6185e8500d7c10d11af6
Ly Mai Le <lymaile@gmail.com>
2014-09-24 14:23:07 -0700

    (feat) ab_test: Submit button sends request to server

--- client/states/ab_test/services/ab_test.service.js
+++ client/states/ab_test/services/ab_test.service.js
@@ -1,9 +1,23 @@
 angular.module('clickaroos.abTest')
 
-.factory('AbTest', ['$http', function($http) {
+.factory('AbTest', ['$http', 'appServerUrl', function($http, appServerUrl) {
   var factory = {};
 
   factory.imagesAndReroutes = [];
+
+  var dataToServer = {
+    user_id: 1, // TODO: Modify from global services
+    campaign_id: 2, // TODO: Modify from global services
+    imagesAndReroutes: factory.imagesAndReroutes
+  };
+
+  // TODO: Send userID and campaignID (from global services)
+  factory.submitImagesAndReroutes = function(imagesAndReroutes) {
+    $http.post(
+      appServerUrl+'/api/ab_tests',
+      dataToServer
+    );
+  };
 
   return factory;
 }])
