22a25cfec0ddcbeb62755236e83ce09a80d43d3b
nrobeR <mr.nrober@gmail.com>
2014-09-26 01:30:23 -0700

    (feat) create a VM serving database This solves the limitted connections issues that exists in ClearDB

--- server/config/dbconnection.js
+++ server/config/dbconnection.js
@@ -1,10 +1,18 @@
 var mysql = require('mysql');
 
+// var connection = mysql.createConnection({
+//   host: 'us-cdbr-azure-west-a.cloudapp.net',
+//   user: 'b017f8a5a6d3e8',
+//   password: '46c0073d',
+//   database: 'ClickagoosDB'
+// });
+
 var connection = mysql.createConnection({
-  host: 'us-cdbr-azure-west-a.cloudapp.net',
-  user: 'b017f8a5a6d3e8',
-  password: '46c0073d',
-  database: 'ClickagoosDB'
+  host: 'clickaroosdb.cloudapp.net',
+  user: 'Clickaroos',
+  password: 'HumanBeans',
+  database: 'clickagoosdb'
 });
 
 module.exports = connection;
+
