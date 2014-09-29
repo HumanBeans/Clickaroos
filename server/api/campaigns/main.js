4560f0e2b768fc3cf92e01af8621e1bd94f0dbc0
Ly Mai Le <lymaile@gmail.com>
2014-09-29 13:20:52 -0700

    (fix) api/campaign/recent serves most recent campaigns

--- server/api/campaigns/main.js
+++ server/api/campaigns/main.js
@@ -6,13 +6,13 @@ var auth = require('../../auth/auth.service');
 
 var router = express.Router();
 
-router.get('/all', controller.all);
 router.get('/', auth.isAuthenticated(), controller.index);
 router.post('/', auth.isAuthenticated(), controller.create);
+router.get('/all', auth.isAuthenticated(), controller.all);
+router.get('/recent', auth.isAuthenticated(), controller.recent);
 router.get('/:campaign', auth.isAuthenticated(), controller.show);
 router.put('/:campaign', auth.isAuthenticated(), controller.update);
 router.delete('/:campaign', auth.isAuthenticated(), controller.remove);
-router.get('/recent', auth.isAuthenticated(), controller.recent);
 
 module.exports = router;
 
