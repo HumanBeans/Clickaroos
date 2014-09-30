39d7abd39aed3e6fbb35af8414c1d47960ca3cd5
Dara Bun <dara209@gmail.com>
2014-09-29 17:08:34 -0700

    (feat) Set up directory for deployment

--- server/config/dbconnection.js
+++ server/config/dbconnection.js
@@ -9,9 +9,16 @@ exports.testingConnection = mysql.createConnection({
 });
 
 // Main DB
+// exports.connection = mysql.createConnection({
+//   host: 'clickaroosdb.cloudapp.net',
+//   user: 'Clickaroos',
+//   password: 'HumanBeans',
+//   database: 'clickagoosdb'
+// });
+
 exports.connection = mysql.createConnection({
-  host: 'clickaroosdb.cloudapp.net',
-  user: 'Clickaroos',
-  password: 'HumanBeans',
+  host: 'us-cdbr-azure-west-a.cloudapp.net',
+  user: 'b017f8a5a6d3e8',
+  password: '46c0073d',
   database: 'clickagoosdb'
 });
