12a0faada2bc8e602b376cdbdc9da8dfa9fd9040
Ly Mai Le <lymaile@gmail.com>
2014-10-02 15:57:33 -0700

    (refactor) Uncomment testingConnection

--- server/config/dbconnection.js
+++ server/config/dbconnection.js
@@ -1,12 +1,12 @@
 var mysql = require('mysql');
 
 // This DB is for testing Purposes
-// exports.testingConnection = mysql.createConnection({
-//   host: 'clickaroosdb.cloudapp.net',
-//   user: 'Clickaroos',
-//   password: 'HumanBeans',
-//   database: 'clickaroosTest'
-// });
+exports.testingConnection = mysql.createConnection({
+  host: 'clickaroosdb.cloudapp.net',
+  user: 'Clickaroos',
+  password: 'HumanBeans',
+  database: 'clickaroosTest'
+});
 
 // Main DB
 exports.connection = mysql.createConnection({
